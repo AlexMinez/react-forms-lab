97f1d153b958902e681ca32e77c854ecadbbcee2
AlexMinez <alex_minihan@outlook.com>
2022-05-10 15:21:39 +1000

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,53 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      UserName: "",
+      PassWord: ""
+    };
   }
+handleUsername = (event) => {
+  this.setState({
+    UserName: event.target.value
+  })
+}
+handlePassword = (event) =>{
+  this.setState({
+    PassWord: event.target.value
+  })
+}
+handleSubmit = (e) => {
+    e.preventDefault()
 
+    if(!this.state.UserName || !this.state.PassWord) return 
+    
+    this.props.handleLogin(this.state)
+}
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input 
+            id="username" 
+            name="username" 
+            type="text"
+            value={this.state.UserName}
+            onChange={this.handleUsername}
+            required />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input 
+            id="password" 
+            name="password" 
+            type="password" 
+            value={this.state.PassWord}
+            onChange={this.handlePassword}
+            required/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+        Character: ""
+    };
   }
-
+maxChars = (event) => {
+  this.setState({
+    Character: event.target.value
+  })
+}
   render() {
+    let CharNum = this.props.maxChars - this.state.Character.length;
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <strong>Your message:{CharNum}</strong>
+        <input 
+        type="text" 
+        name="message" 
+        id="message" 
+        value={this.state.Character} 
+        onChange={this.maxChars} />
       </div>
     );
   }
